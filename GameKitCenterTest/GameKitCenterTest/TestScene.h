e98b0bcfd52a40bce5d2a409a5a359eada36996b
Hasyimi <hasyimi@terato.com>
2012-10-23 09:08:04 +0800

    Add: Initial test project

---
+++ GameKitCenterTest/GameKitCenterTest/TestScene.h
@@ -0,0 +1,13 @@
+//
+//  TestScene.h
+//  GameKitCenterTest
+//
+//  Created by Hasyimi Bahrudin on 10/23/12.
+//
+//
+
+#import "cocos2d.h"
+
+@interface TestScene : CCScene
+
+@end
---
+++ GameKitCenterTest/GameKitCenterTest/TestScene.m
@@ -0,0 +1,13 @@
+//
+//  TestScene.m
+//  GameKitCenterTest
+//
+//  Created by Hasyimi Bahrudin on 10/23/12.
+//
+//
+
+#import "TestScene.h"
+
+@implementation TestScene
+
+@end
